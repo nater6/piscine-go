91e53e4ac7c4954b3945ff46f78a4fa2f8cadb0f
nater68 <n.d.russell@hotmail.com>
2021-11-30 17:02:25 +0000

    Empty main

--- isnegative.go
+++ isnegative.go
@@ -1,13 +1,8 @@
 package main
 
-import (
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 func main() {
-	IsNegative(1)
-	IsNegative(0)
-	IsNegative(-1)
 }
 
 func IsNegative(nb int) {
