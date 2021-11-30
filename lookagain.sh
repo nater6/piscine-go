c85990de88dfb2d5aa5294b5afb9df7613df7a41
nater68 <n.d.russell@hotmail.com>
2021-11-30 14:03:53 +0000

    ALl files

--- lookagain.sh
+++ lookagain.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
+ find -name "*.sh" 
+ find -name "*.sh" |rev
+ find -name "*.sh" |rev| cut -d "/" -f1 
+ find -name "*.sh" |rev| cut -d "/" -f1 | rev 
+ find -name "*.sh" |rev| cut -d "/" -f1 | rev | sed 's/.sh//g' 
  find -name "*.sh" |rev| cut -d "/" -f1 | rev | sed 's/.sh//g' | sort -n -r 
