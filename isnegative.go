0a16a002cc0af1fdd5622f42a6c3eb6df586dcb8
nater68 <n.d.russell@hotmail.com>
2021-11-30 17:19:59 +0000

    Correct package

--- isnegative.go
+++ isnegative.go
@@ -1,13 +1,16 @@
-package piscine
+package main
 
-import "fmt"
+import "github.com/01-edu/z01"
+
+func main() {
+}
 
 func IsNegative(nb int) {
 	if nb < 0 {
-		fmt.Println('T')
-		fmt.Println('\n')
+		z01.PrintRune('T')
+		z01.PrintRune('\n')
 	} else {
-		fmt.Println('F')
-		fmt.Println('\n')
+		z01.PrintRune('F')
+		z01.PrintRune('\n')
 	}
 }
