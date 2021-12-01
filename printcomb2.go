bed275b0105f9bb25de953a97f91e175c6eb76bc
nater68 <n.d.russell@hotmail.com>
2021-12-01 15:09:30 +0000

    Nested for loops

--- printcomb2.go
+++ printcomb2.go
@@ -7,7 +7,7 @@ func PrintComb2() {
 		for j := '0'; j <= '8'; j++ {
 			for k := '0'; k <= '9'; k++ {
 				for l := '0'; l <= '9'; l++ {
-					if (i <= k && l > j) {
+					if i <= k && l > j {
 
 						z01.PrintRune(i)
 						z01.PrintRune(j)
