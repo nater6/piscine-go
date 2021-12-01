b7111de1fe6b05220192df97b5b230b38b81b0ae
nater68 <n.d.russell@hotmail.com>
2021-12-01 12:38:35 +0000

    Num counter

---
+++ printcomb.go
@@ -0,0 +1,27 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func printcomb() {
+	var i rune = '0'
+	var j rune = '0'
+	var k rune = '0'
+
+	for i <= 9 {
+		if i < j && (j < k) {
+			z01.PrintRune(i)
+			z01.PrintRune(j)
+			z01.PrintRune(k)
+			z01.PrintRune(',')
+			z01.PrintRune(' ')
+		} else if i >= j {
+			j++
+		} else if j >= k {
+			k++
+		} else {
+			i++
+			j = 0
+			k = 0
+		}
+	}
+}
