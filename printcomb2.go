911fee541b1edc69b1e4828db3d4fa42522715a9
Your Name <you@example.com>
2021-12-01 15:16:05 +0000

    With 10s

--- printcomb2.go
+++ printcomb2.go
@@ -8,20 +8,17 @@ func PrintComb2() {
 			for k := '0'; k <= '9'; k++ {
 				for l := '0'; l <= '9'; l++ {
 					if i <= k && l > j {
-
 						z01.PrintRune(i)
 						z01.PrintRune(j)
 						z01.PrintRune(' ')
 						z01.PrintRune(k)
 						z01.PrintRune(l)
-
 						if !(i == '9' && j == '8' && k == '9' && l == '9') {
 							z01.PrintRune(',')
 							z01.PrintRune(' ')
 						} else {
 							z01.PrintRune('\n')
 						}
-
 					}
 				}
 			}
