eabcf63010bcacfc323cefeae4c1acf13cb88f83
nater68 <nater68@noreply.git.learn.01founders.co>
2021-12-01 17:58:05 +0000

    Update 'printcomb2.go'

--- printcomb2.go
+++ printcomb2.go
@@ -4,7 +4,7 @@ import "github.com/01-edu/z01"
 
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
-		for j := '0'; j <= '8'; j++ {
+		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
 				for l := '0'; l <= '9'; l++ {
 					if i < k || (k == i && l > j) {
