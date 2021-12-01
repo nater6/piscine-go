889efa8943409bffba2458284f00e83219ae023d
nater68 <n.d.russell@hotmail.com>
2021-12-01 12:57:09 +0000

    Nested for loops

--- printcomb.go
+++ printcomb.go
@@ -3,25 +3,16 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	var i rune = '0'
-	var j rune = '0'
-	var k rune = '0'
-
-	for i <= 9 {
-		if i < j && (j < k) {
-			z01.PrintRune(i)
-			z01.PrintRune(j)
-			z01.PrintRune(k)
-			z01.PrintRune(',')
-			z01.PrintRune(' ')
-		} else if i >= j {
-			j++
-		} else if j >= k {
-			k++
-		} else {
-			i++
-			j = 0
-			k = 0
+	for i := '0'; i <= 9; i++ {
+		for j := i + 1; j <= 9; j++ {
+			for k := j + 1; k <= 9; k++ {
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				z01.PrintRune(k)
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			}
 		}
 	}
+	z01.PrintRune('\n')
 }
