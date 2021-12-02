32eb7cfe2b1a24eaea78fa3ceab501a22b2bb8f2
Your Name <you@example.com>
2021-12-02 17:53:18 +0000

    Formatted

--- strrev.go
+++ strrev.go
@@ -1,12 +1,12 @@
 package piscine
 
-func StrRev(s string) string{
+func StrRev(s string) string {
 	rune1 := []rune(s)
-	for i,j := 0, len(s)-1; i<j; i,j = i+1, j-1 {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		x := rune1[i]
 		rune1[i] = rune1[j]
 		rune1[j] = x
 	}
 	y := string(rune1)
 	return y
-	}
+}
