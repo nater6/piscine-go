59473f32cdff3f1e905b7477b890e4dc05c722c8
Your Name <you@example.com>
2021-12-05 16:36:58 +0000

    a

--- iterativepower.go
+++ iterativepower.go
@@ -2,14 +2,17 @@ package piscine
 
 func IterativePower(nb int, power int) int {
 	result1 := 0
+	x := nb
 	if power > 0 {
-		for i := 0; i < power; i++ {
-			result1 = nb * nb
+		for i := 0; i < power-1; i++ {
+			x = x * nb
+			result1 = x
 		}
+		return result1
 	} else if power == 0 {
 		result1 = 1
+		return result1
 	} else {
 		return 0
 	}
-	return result1
 }
