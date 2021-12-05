48165f9a7f87d2acad826a81196eb2ec289aed0a
Your Name <you@example.com>
2021-12-05 15:53:03 +0000

    a

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,8 +2,8 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	result1 := 1
-	if nb > 0 {
-		for i := 1; i < nb+1 ; i++ {
+	if nb > 0 && nb < 21{
+		for i := 1; i <= nb; i++ {
 			result1 = result1 * i
 		}
 		return result1
