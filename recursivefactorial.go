81f7aaee2dd8a74001a30efcebee018dd17f0949
Your Name <you@example.com>
2021-12-05 16:20:04 +0000

    rec fact!

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -2,9 +2,9 @@ package piscine
 
 func RecursiveFactorial(nb int) int {
 	result1 := 0
-	if nb == 0 && nb < 21 {
+	if nb == 0 {
 		result1 = 1
-	} else if nb > 0 {
+	} else if nb > 0 && nb < 21 {
 		result1 = nb * RecursiveFactorial(nb-1)
 	} else {
 		return 0
