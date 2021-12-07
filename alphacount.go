ad79f0c63a6b14fa0c0115f46165fad83122e6b1
Your Name <you@example.com>
2021-12-07 18:03:16 +0000

    counter

--- alphacount.go
+++ alphacount.go
@@ -3,11 +3,11 @@ package piscine
 func AlphaCount(s string) int {
 	rune1 := []rune(s)
 
-	i := 0
-	for i < len(s) {
+	x := 0
+	for i := 0; i < len(s); i++ {
 		if (rune1[i] >= 'a' && rune1[i] <= 'z') || (rune1[i] >= 'A' && rune1[i] <= 'Z') {
-			i++
+			x++
 		}
 	}
-	return i
+	return x
 }
