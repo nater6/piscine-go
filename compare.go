d862758c5a0d2d5107cf0efac4ce34f2a0f1daff
Your Name <you@example.com>
2021-12-07 17:19:04 +0000

    Rune reader

---
+++ compare.go
@@ -0,0 +1,26 @@
+package piscine
+
+func Compare(a, b string) int {
+	compareA := []rune(a)
+	compareB := []rune(b)
+
+	i := 0
+	for i < len(compareA) {
+		i++
+	}
+
+	j := 0
+
+	for j < len(compareB) {
+		j++
+	}
+	{
+		if i == j {
+			return 0
+		} else if i < j {
+			return -1
+		} else {
+			return 1
+		}
+	}
+}
