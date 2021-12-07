508784aef8621adba8508ca78463a03207a3c08b
Your Name <you@example.com>
2021-12-07 13:44:23 +0000

    Square root

--- sqrt.go
+++ sqrt.go
@@ -4,13 +4,13 @@ func Sqrt(nb int) int {
 	var result int
 
 	if nb > 0 {
-		for x:=0; x < nb; x++ {
+		for x := 0; x < nb; x++ {
 			if nb == x*x {
 				result = x
-			} 
-		} 
+			}
+		}
 	} else {
 		result = 0
-	} 
+	}
 	return result
 }
