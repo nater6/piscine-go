2acd455c8aab66ef7c99166211d079cd91ec31e5
Your Name <you@example.com>
2021-12-08 16:01:39 +0000

    index

--- isalpha.go
+++ isalpha.go
@@ -7,6 +7,7 @@ func IsAlpha(s string) bool {
 		if rune1[i] >= 'a' && rune1[i] <= 'z' {
 		} else if rune1[i] >= 'A' && rune1[i] <= 'Z' {
 		} else if rune1[i] == ' ' {
+		} else if rune1[i] >= '0' && rune1[i] <= '9' {
 		} else {
 			return false
 		}
