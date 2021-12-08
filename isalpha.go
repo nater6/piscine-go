d70883bd88f3266071d4ce24c624d23c830381bd
Your Name <you@example.com>
2021-12-08 15:59:14 +0000

    index

--- isalpha.go
+++ isalpha.go
@@ -4,7 +4,10 @@ func IsAlpha(s string) bool {
 	rune1 := []rune(s)
 
 	for i := 0; i < len(s); i++ {
-		if (rune1[i] < 'a' || (rune1[i] > 'z' && rune1[i] < 'A') || rune1[i] > 'Z') && rune1[i] != ' ' {
+		if rune1[i] >= 'a' && rune1[i] <= 'z' {
+		} else if rune1[i] >= 'A' && rune1[i] <= 'Z' {
+		} else if rune1[i] == ' ' {
+		} else {
 			return false
 		}
 	}
