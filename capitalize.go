448ebf5c9c6f1919511c5d97b4ab4b2ee45734c1
Your Name <you@example.com>
2021-12-08 18:52:21 +0000

    cap

--- capitalize.go
+++ capitalize.go
@@ -2,18 +2,22 @@ package piscine
 
 func Capitalize(s string) string {
 	r := []rune(s)
+	l := 0
 
 	for i := 0; i < len(s); i++ {
-		if r[i] == ' ' {
-			if r[i+1] >= 'a' && r[i] <= 'z' {
-				r[i+1] -= 32
-			}
-			for j := i; r[j] == ' '; j++ {
-				if r[j] >= 'A' && r[j] <= 'Z' {
-					r[j] += 32
-				}
-			}
-		}
+		if l == 0 && (r[i] >= 'a' && r[i] <= 'z') {
+			r[i] = r[i] - 32
+			l++
+		} else if l == 0 && ((r[i] >= 'A' && r[i] <= 'Z') || (r[i] >= '0' && r[i] <= '9')) {
+			l++
+		} else if (r[i] >= 'A' && r[i] <= 'Z') {
+			r[i] = r[i] + 32
+			l++
+		} else if (r[i] >= '0' && r[i] <= '9') || (r[i] >= 'a' && r[i] <= 'z')  {
+			l++
+		} else {
+			l=0
+		} 
 	}
 	return string(r)
 }
