ca5103f9c45ba6b3e10efc59e3954abf11581abe
Your Name <you@example.com>
2021-12-08 16:23:30 +0000

    index

--- isprintable.go
+++ isprintable.go
@@ -4,7 +4,7 @@ func IsPrintable(s string) bool {
 	r := []rune(s)
 
 	for i := 0; i < len(s); i++ {
-		if (r[i] > 32 || r[i] < 127) && r[i] != '\\' {
+		if r[i] > 32 {
 		} else {
 			return false
 		}
