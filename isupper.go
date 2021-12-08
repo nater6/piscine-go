03fa85b90d46e6bf074c4583564240095c2034e2
Your Name <you@example.com>
2021-12-08 15:27:49 +0000

    index

--- isupper.go
+++ isupper.go
@@ -2,11 +2,10 @@ package piscine
 
 func IsUpper(s string) bool {
 	rune1 := []rune(s)
-	for i:=0; i<len(s); i++ {
+	for i := 0; i < len(s); i++ {
 		if rune1[i] > 'Z' || rune1[i] < 'A' {
 			return false
 		}
 	}
 	return true
 }
-
