d186b9e45fda29daf5adf692f8fd6b6da2dd767d
Your Name <you@example.com>
2021-12-09 17:46:50 +0000

    A

--- revparams/main.go
+++ revparams/main.go
@@ -8,23 +8,19 @@ import (
 
 func main() {
 	args := os.Args
-	
-	 for i, j := 0, len(args)-1; i < j; i, j= i+1, j-1 {
-		 x := args[i]
-		 args[i] = args[j]
-		 args[j] = x
-	 }
 
-	
+	for i, j := 0, len(args)-1; i < j; i, j = i+1, j-1 {
+		x := args[i]
+		args[i] = args[j]
+		args[j] = x
+	}
 
-	 for a := 0; a < len(args)-1; a++ {
-		 word := args[a]
-		 for _, letter := range word {
-			
-			 z01.PrintRune(rune(letter))
-		 }
-		 z01.PrintRune('\n')
+	for a := 0; a < len(args)-1; a++ {
+		word := args[a]
+		for _, letter := range word {
+			z01.PrintRune(rune(letter))
+		}
+		z01.PrintRune('\n')
 
-	 }
-	 
+	}
 }
