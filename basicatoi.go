0b49876af7293e4c8fcae54fdfc89868d6172582
Your Name <you@example.com>
2021-12-09 12:14:44 +0000

    basic atoi

--- basicatoi.go
+++ basicatoi.go
@@ -1,16 +1,13 @@
 package piscine
 
 func BasicAtoi(s string) int {
-	x:=0
+	x := 0
 	r := []rune(s)
 
-	for i:=0; i<len(s); i++ {
-		y := r[i]-48
-		x = x*10
+	for i := 0; i < len(s); i++ {
+		y := r[i] - 48
+		x = x * 10
 		x += int(y)
 	}
 	return x
 }
-
-	
-
