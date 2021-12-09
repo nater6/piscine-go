432d811e1bae92fe2a58e3b58926a8a0e9631982
Your Name <you@example.com>
2021-12-09 17:46:30 +0000

    A

---
+++ revparams/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args
+	
+	 for i, j := 0, len(args)-1; i < j; i, j= i+1, j-1 {
+		 x := args[i]
+		 args[i] = args[j]
+		 args[j] = x
+	 }
+
+	
+
+	 for a := 0; a < len(args)-1; a++ {
+		 word := args[a]
+		 for _, letter := range word {
+			
+			 z01.PrintRune(rune(letter))
+		 }
+		 z01.PrintRune('\n')
+
+	 }
+	 
+}
