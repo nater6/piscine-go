5594027d299642a98d1910be60780294e618d7ab
Your Name <you@example.com>
2021-12-09 22:06:39 +0000

    All Files

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -19,10 +19,10 @@ func PrintNbrInOrder(n int) {
 	for i := 0; i < len(numbers)-1; i++ {
 		for j := 0; j < len(numbers)-1; j++ {
 			if numbers[j] > numbers[j+1] {
-			numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+				numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
 			}
-	}
 		}
+	}
 
 	for _, digit := range numbers {
 		z01.PrintRune(rune(digit) + 48)
