62f28506edb800411a969c25f0a787669d8077e0
Your Name <you@example.com>
2021-12-09 13:03:40 +0000

    trimmed

--- trimatoi.go
+++ trimatoi.go
@@ -6,20 +6,15 @@ func TrimAtoi(s string) int {
 	check := false
 	noDig := true
 
-	for x := 0; (r[x] >= '0' && r[x] <= '9') || x < len(s)-1; x++ {
-		if r[x] == '-' {
-			check = true
-		}
-	}
-
 	for i := 0; i < len(s); i++ {
 		if r[i] >= '0' && r[i] <= '9' {
-			if r[i] >= '0' && r[i] <= '9' {
-				rDigit := r[i] - 48
-				intValue = intValue * 10
-				intValue = intValue + int(rDigit)
-				noDig = false
-			}
+			rDigit := r[i] - 48
+			intValue = intValue * 10
+			intValue = intValue + int(rDigit)
+			noDig = false
+		} else if r[i] == '-' && intValue == 0 {
+			check = true
+		} else {
 		}
 	}
 
