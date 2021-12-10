0a15c40aa9aa5bfadae6b931defbd86502625397
Your Name <you@example.com>
2021-12-10 19:19:14 +0000

    /maker

--- concatparams.go
+++ concatparams.go
@@ -3,6 +3,9 @@ package piscine
 func ConcatParams(args []string) string {
 	var str string
 	for i := 0; i < len(args); i++ {
+		if i == len(args)-1 {
+			str = str + string(args[i])
+		}
 		str = str + string(args[i]) + "\n"
 	}
 	return str
