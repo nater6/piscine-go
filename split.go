365ec36ae82c6792db6f934907fee25fdc49515c
Your Name <you@example.com>
2021-12-12 16:28:23 +0000

    Printer

--- split.go
+++ split.go
@@ -5,16 +5,16 @@ func Split(s, sep string) []string {
 	wordIndex := 0
 
 	if len(s) >= len(sep) {
-		for i := 0; i < len(s); i++ {
+		for i := 0; i <= len(s)-len(sep); i++ {
 			if sep == s[i:i+len(sep)] {
 				if s[wordIndex:i] == "" {
-					wordIndex = i + 1
+					wordIndex = i + len(sep)
 					continue
 				}
 				separated = append(separated, s[wordIndex:i])
-				wordIndex = i + 1
-			} else if i == len(s)-1 {
-				separated = append(separated, s)
+				wordIndex = i + len(sep)
+			} else if i == len(s)-len(sep) {
+				separated = append(separated, s[wordIndex:])
 			}
 		}
 	}
