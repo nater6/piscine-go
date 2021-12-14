7981adc785c37bb614cb622e9269d1ee4371ff97
Your Name <you@example.com>
2021-12-14 14:48:23 +0000

    Map func

--- issorted.go
+++ issorted.go
@@ -1,11 +1,16 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
+	sortedTester := []int{}
 	for i := 0; i < len(a)-1; i++ {
-		// if f(a[i], a[i+1]) > 0 {
-		// 	return false
-		// }
-		if a[i] > a[i+1] {
+		if f(a[i], a[i+1]) > 0 {
+			sortedTester = append(sortedTester, 0)
+		} else if f(a[i], a[i+1]) < 0 {
+			sortedTester = append(sortedTester, 1)
+		}
+	}
+	for j := 0; j < len(sortedTester)-1; j++ {
+		if sortedTester[j] != sortedTester[j+1] {
 			return false
 		}
 	}
