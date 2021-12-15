74ec99d0cdf00a03db689feb00f33c812a853aa0
Your Name <you@example.com>
2021-12-15 14:00:54 +0000

    Print fmt

--- comcheck/main.go
+++ comcheck/main.go
@@ -9,10 +9,9 @@ func main() {
 	argue := os.Args
 	args := argue[1:]
 
-	for i:=0; i<len(args); i++ {
+	for i := 0; i < len(args); i++ {
 		if args[i] == "01" || args[i] == "galaxy" || args[i] == "galaxy 01" {
 			fmt.Println("Alert!!!")
 		}
 	}
-
 }
