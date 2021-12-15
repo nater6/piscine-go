37d12c906e25998af1bf18b66389da7e715bffee
Your Name <you@example.com>
2021-12-15 13:25:28 +0000

    Abort

---
+++ abort.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Abort(a, b, c, d, e int) int {
+	intSlice := []int{a, b, c, d, e}
+	for i := 0; i < len(intSlice)-1; i++ {
+		for j := 0; j < len(intSlice)-1; j++ {
+			if intSlice[j] > intSlice[j+1] {
+				intSlice[j], intSlice[j+1] = intSlice[j+1], intSlice[j]
+			}
+		}
+	}
+	return intSlice[3]
+}
