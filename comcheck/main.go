652355b788569aaba487d2732ea8e22ac1e9d613
Your Name <you@example.com>
2021-12-15 14:00:28 +0000

    Print fmt

---
+++ comcheck/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	argue := os.Args
+	args := argue[1:]
+
+	for i:=0; i<len(args); i++ {
+		if args[i] == "01" || args[i] == "galaxy" || args[i] == "galaxy 01" {
+			fmt.Println("Alert!!!")
+		}
+	}
+
+}
