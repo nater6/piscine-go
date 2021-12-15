e746c2a5548fd4f5499235bac17b252f04f1de56
Your Name <you@example.com>
2021-12-15 13:00:05 +0000

    rot 14

--- rot14.go
+++ rot14.go
@@ -1,9 +1,21 @@
 package piscine
 
 func Rot14(s string) string {
-	for _, letter := range s {
-		letter = letter + 14
+	r := []rune(s)
+
+	for i := 0; i < len(s); i++ {
+		if (r[i] >= 'a' && r[i] <= 'z') || (r[i] >= 'A' && r[i] <= 'Z') {
+			if (r[i] <= 76 && r[i] >= 65) || (r[i] <= 108 && r[i] >= 97) {
+				r[i] = r[i] + 14
+			} else if r[i] <= 90 && r[i] >= 77 {
+				x := 90 - r[i]
+				r[i] = 65 + x + 14
+			} else if r[i] <= 122 && r[i] >= 109 {
+				y := 122 - r[i]
+				r[i] = 97 + y + 14
+			}
+		}
 	}
 
-	return s
+	return string(r)
 }
