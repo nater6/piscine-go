b1b37193c432e7f8324bca3d91f095fb2ca10978
Your Name <you@example.com>
2021-12-15 17:12:11 +0000

    Print fmt

---
+++ enigma.go
@@ -0,0 +1,21 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	// Arguement place holders
+	a1 := a
+	b1 := b
+	c1 := c
+	d1 := d
+
+	// For a
+	**a = b1
+
+	// for b
+	b = ***d1
+
+	// for c
+	****c = a1
+
+	// for d
+	d = ***c1
+}
