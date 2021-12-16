4ddf379d2f23d27263ee75f2dd5ba96056f13edc
Your Name <you@example.com>
2021-12-16 14:51:27 +0000

    doop

--- doop/main.go
+++ doop/main.go
@@ -18,25 +18,25 @@ func main() {
 			return
 		}
 	}
-	//Check the 3rd arg is an int
+	// Check the 3rd arg is an int
 	for _, digit := range args[2] {
 		if !(digit >= '0' && digit <= '9') {
 			return
 		}
 	}
-	//Check that the middle arg is an operator
+	// Check that the middle arg is an operator
 	if !(args[1] == "+" || args[1] == "-" || args[1] == "/" || args[1] == "%" || args[1] == "*") {
 		return
 	}
-	//Check that modulo isnt with 0
+	// Check that modulo isnt with 0
 	if args[1] == "%" && args[2] == "0" {
 		print("No modulo by 0")
 	}
-	//Check division isnt with a zero
+	// Check division isnt with a zero
 	if args[1] == "/" && args[2] == "0" {
 		print("No division by 0")
 	}
-	//Check number is within int64 range
+	// Check number is within int64 range
 
 	int1, _ := strconv.Atoi(args[0])
 	int2, _ := strconv.Atoi(args[2])
@@ -60,5 +60,4 @@ func main() {
 		result = int1 % int2
 	}
 	print(result)
-
 }
