f7395f68e2ce628efb14fe84dfc81029d57d11c9
Your Name <you@example.com>
2021-12-16 11:46:42 +0000

    Door

---
+++ fixthemain/main.go
@@ -0,0 +1,54 @@
+package main
+
+import "github.com/01-edu/z01"
+
+type Door struct {
+	state string
+}
+
+func PrintStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func CloseDoor(Door *Door) {
+	PrintStr("Door Closing...")
+	Door.state = "CLOSE"
+}
+
+func OpenDoor(Door *Door) {
+	PrintStr("Door Opening...")
+	Door.state = "OPEN"
+}
+
+func IsDoorOpen(Door *Door) bool {
+	PrintStr("is the Door opened ?")
+	if Door.state == "OPEN" {
+		return true
+	} else {
+		return false
+	}
+}
+
+func IsDoorClose(Door *Door) bool {
+	PrintStr("is the Door closed ?")
+	if Door.state == "CLOSE" {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	door := &Door{}
+
+	OpenDoor(door)
+	if IsDoorClose(door) {
+		OpenDoor(door)
+	}
+	if IsDoorOpen(door) {
+		CloseDoor(door)
+	}
+}
