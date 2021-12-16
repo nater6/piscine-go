ddbf580822278ca9155ca66724a9782a5dd47276
Your Name <you@example.com>
2021-12-16 12:37:56 +0000

    Atoi

--- atoi.go
+++ atoi.go
@@ -1,13 +1,14 @@
 package piscine
 
 func Atoi(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+
 	r := s
 	rune1 := s[1:]
 	negative := false
 
-	if len(s) == 0 {
-		return 0
-	}
 	// Check all vakues are digits
 	if r[0] == '-' || r[0] == '+' {
 		for i := 1; i < len(s); i++ {
