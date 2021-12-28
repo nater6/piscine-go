b2e1a5b96e0b0938b50bbfcdc18cc2480930b120
Your Name <you@example.com>
2021-12-28 17:54:23 +0000

    Param sorter

---
+++ sortparams/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args[1:]
+	rSlice := []rune{}
+
+	for i := 0; i < len(args); i++ {
+		for _, r := range args[i] {
+			rSlice = append(rSlice, r)
+		}
+	}
+
+	for i := 0; i < len(rSlice); i++ {
+		for j := i; j < len(rSlice); j++ {
+			if rSlice[i] > rSlice[j] {
+				rSlice[i], rSlice[j] = rSlice[j], rSlice[i]
+			}
+		}
+	}
+
+	for _, print := range rSlice {
+		z01.PrintRune(print)
+		z01.PrintRune('\n')
+	}
+}
